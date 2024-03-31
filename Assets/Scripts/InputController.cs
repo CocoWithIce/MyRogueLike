d91f6280dce7bcc77b35bf06420c778ee57d5c36
CocoWithIce <957790466@qq.com>
2024-03-31 20:09:40 +0800

    add InputController

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,34 +1,65 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class InputController : MonoBehaviour
 {
     public NavMeshAgent Agent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Dictionary<KeyCode, Action> InputActionMaps = new ();
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        LoadDefaultInputSettings();
+    }
+    
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        InputCheck();
+    }
+    
+    private void InputCheck()
+    {
+        foreach (var pair in InputActionMaps)
         {
-            RaycastHit ray;
-            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(camRay.origin, camRay.direction, out ray, 1000f))
+            if (Input.GetKeyDown(pair.Key))
             {
-                if (ray.transform)
+                pair.Value.Invoke();
+            }
+        }
+    }
+
+    private void LoadDefaultInputSettings()
+    {
+        InputActionMaps.Add(KeyCode.Mouse1,MoveToMousePosition);
+        InputActionMaps.Add(KeyCode.Q,()=>Debug.Log("Skill1 ==> Q"));
+        InputActionMaps.Add(KeyCode.W,()=>Debug.Log("Skill2 ==> W"));
+        InputActionMaps.Add(KeyCode.E,()=>Debug.Log("Skill3 ==> E"));
+        InputActionMaps.Add(KeyCode.R,()=>Debug.Log("Skill4 ==> R"));
+        InputActionMaps.Add(KeyCode.T,()=>Debug.Log("Skill5 ==> T"));
+        InputActionMaps.Add(KeyCode.Alpha1,()=>Debug.Log("Util1 ==> 1"));
+        InputActionMaps.Add(KeyCode.Alpha2,()=>Debug.Log("Util2 ==> 2"));
+        InputActionMaps.Add(KeyCode.Alpha3,()=>Debug.Log("Util3 ==> 3"));
+        InputActionMaps.Add(KeyCode.Alpha4,()=>Debug.Log("Util4 ==> 4"));
+        InputActionMaps.Add(KeyCode.Alpha5,()=>Debug.Log("Util5 ==> 5"));
+        InputActionMaps.Add(KeyCode.Tab,()=>Debug.Log("Minimap ==> Tab"));
+        InputActionMaps.Add(KeyCode.C,()=>Debug.Log("PlayerStats ==> C"));
+        InputActionMaps.Add(KeyCode.I,()=>Debug.Log("Inventory ==> I"));
+        InputActionMaps.Add(KeyCode.P,()=>Debug.Log("Portal ==> P"));
+        InputActionMaps.Add(KeyCode.Escape,()=>Debug.Log("Quit ==> Escape"));
+    }
+    
+    private void MoveToMousePosition()
+    {
+        RaycastHit ray;
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(camRay.origin, camRay.direction, out ray, 1000f))
+        {
+            if (ray.transform)
+            {
+                if (ray.transform.gameObject.layer == LayerMask.NameToLayer("Floor"))
                 {
-                    if (ray.transform.gameObject.layer == LayerMask.NameToLayer("Floor"))
-                    {
-                        Agent.SetDestination(ray.point);
-                    }
+                    Agent.SetDestination(ray.point);
                 }
             }
         }
